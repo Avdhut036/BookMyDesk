e945630c3af423f031d295bff67cea28577188e5
sid-ghost <siddharth.siddhatech@gmail.com>
2024-07-31 19:28:29 +0530

    completed basic navbars for admin page

--- FRONTEND/BookMyDesk/src/Pages/Roles/Admin/AdminPage.jsx
+++ FRONTEND/BookMyDesk/src/Pages/Roles/Admin/AdminPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import AdminSideNav from "../../../Components/adminPage/Navbar/side_nav/AdminSideNav";
 import AdminTopNav from "../../../Components/adminPage/Navbar/top_nav/AdminTopNav";
-import styles from "./AdminPage.module.css"; // Create this CSS module file for layout styling
+import styles from "./AdminPage.module.css"; // Ensure CSS module has correct layout styles
 
 const AdminPage = () => {
   return (
     <div className={styles.pageContainer}>
       <AdminTopNav />
-      <div className={styles.content}>
+      <div className={styles.mainContentWrapper}>
         <AdminSideNav />
         <div className={styles.mainContent}>
           {/* Add main content here */}
