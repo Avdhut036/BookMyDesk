395376d6402aaf9243cab9b5b0cb5b23a23cf80d
sid-ghost <siddharth.siddhatech@gmail.com>
2024-07-31 15:35:29 +0530

    Top, Side Nav for Admin Page
    added top nav and side nav for admin page, but the top nav needs to be fixed as its overlapping side nav

--- FRONTEND/BookMyDesk/src/Components/adminPage/Navbar/side_nav/AdminSideNav.js
+++ FRONTEND/BookMyDesk/src/Components/adminPage/Navbar/side_nav/AdminSideNav.js
@@ -1,13 +1,133 @@
 import styles from "./AdminSideNav.module.css"; 
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import PersonIcon from '@mui/icons-material/Person';
+import ViewComfyIcon from '@mui/icons-material/ViewComfy';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import Logo from '../../../../assets/SSPL-LOGO-1.ico';
 
+const AdminSideNav = ({ open, onClose }) => {
+  const [openAdminDashboard, setOpenAdminDashboard] = React.useState(false);
+  const [openFloorLayout, setOpenFloorLayout] = React.useState(false);
 
-const AdminSideNav = () => {
-  
-  return (
-    <div>
-        heyy
-    </div>
+  const handleToggleAdminDashboard = () => {
+    setOpenAdminDashboard(!openAdminDashboard);
+  };
+
+  const handleToggleFloorLayout = () => {
+    setOpenFloorLayout(!openFloorLayout);
+  };
+
+  const DrawerList = (
+    
+    <Box sx={{ width: 250 }} role="presentation">
+      <Box sx={{ textAlign: 'center', padding: '16px' }}>
+        <img src={Logo} alt="Company Logo" className={styles.logo} />
+      </Box>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton onClick={handleToggleAdminDashboard}>
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Admin Dashboard" />
+            {openAdminDashboard ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+        </ListItem>
+        {openAdminDashboard && (
+          <Box component="div" className={styles.subMenu}>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary="Dashboard Item 1" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary="Dashboard Item 2" />
+              </ListItemButton>
+            </ListItem>
+          </Box>
+        )}
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <EventNoteIcon />
+            </ListItemIcon>
+            <ListItemText primary="Your Bookings" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <PersonIcon />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton onClick={handleToggleFloorLayout}>
+            <ListItemIcon>
+              <ViewComfyIcon />
+            </ListItemIcon>
+            <ListItemText primary="Floor Layout" />
+            {openFloorLayout ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+        </ListItem>
+        {openFloorLayout && (
+          <Box component="div" className={styles.subMenu}>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary="Floor Layout Item 1" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary="Floor Layout Item 2" />
+              </ListItemButton>
+            </ListItem>
+          </Box>
+        )}
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <CloudUploadIcon />
+            </ListItemIcon>
+            <ListItemText primary="Upload Data" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
+    </Box>
   );
-};
+
+  return (
+    <Drawer
+      sx={{
+        width: 250,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: 250,
+          boxSizing: 'border-box',
+        },
+      }}
+      variant="persistent"
+      anchor="left"
+      open={open}
+    >
+      {DrawerList}
+    </Drawer>
+  );
+}
 
 export default AdminSideNav;
--- FRONTEND/BookMyDesk/src/Pages/Roles/Admin/AdminPage.jsx
+++ FRONTEND/BookMyDesk/src/Pages/Roles/Admin/AdminPage.jsx
@@ -1,13 +1,28 @@
-import AddUser from "../../../Components/adminPage/AddUser";
+import AdminSideNav from "../../../Components/adminPage/Navbar/side_nav/AdminSideNav";
+import AdminTopNav from "../../../Components/adminPage/Navbar/top_nav/AdminTopNav";
 import styles from "./AdminPage.module.css"; 
-
+import React, { useState } from 'react';
+import { CssBaseline, Box } from '@mui/material';
 
 const AdminPage = () => {
-  
+  const [sideNavOpen, setSideNavOpen] = useState(true);
+
+  const handleDrawerToggle = () => {
+    setSideNavOpen(!sideNavOpen);
+  };
+
   return (
-    <div>
-       <AddUser/> 
-    </div>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AdminSideNav open={sideNavOpen} onClose={handleDrawerToggle} />
+      <Box
+        component="main"
+    
+      >
+        <AdminTopNav onMenuClick={handleDrawerToggle} />
+        {/* Your other content here */}
+      </Box>
+    </Box>
   );
 };
 
